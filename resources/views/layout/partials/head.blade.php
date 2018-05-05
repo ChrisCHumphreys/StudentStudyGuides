165fd71b4e30af72d21fa805de406c17fc2598d2
Chris Humphreys <geochris35>
2018-05-05 15:20:07 -0500

    initial commit

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<meta name="description" content="">
+
+<meta name="author" content="">
+
+
+
+<title>Album example for Bootstrap</title>
+
+
+
+<!-- Bootstrap core CSS -->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUIyJ" crossorigin="anonymous">
+
+
+
+
+<!-- Custom styles for this template -->
+
+<link href="../../../../public/css/lux.css" rel="stylesheet">
+
